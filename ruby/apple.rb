0c2df12f9760434cfd13bf8797d629b8e1ec6363
ogi0116 <135587780+ogi0116@users.noreply.github.com>
2023-06-07 08:30:57 +0000

    [Finish]2-12

---
+++ ruby/apple.rb
@@ -0,0 +1,11 @@
+apple = "Yamanashi"
+
+if apple == "Aomori"
+  puts "このリンゴは青森県産です。"
+
+elsif apple == "Nagano"
+  puts "このリンゴは青森産ではなく、長野産です。"
+
+else
+  puts "このリンゴは青森産でも長野産でもありません。"
+end
---
+++ ruby/new.rb
@@ -0,0 +1,37 @@
+#インスタンス
+class Car
+
+  def move(direction,distance)
+    self.turn(direction)
+    self.turn(distance)
+  end
+
+  def turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+car = Car.new
+car.move("右",5)
+
+#クラス
+class Car
+  def self.turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+end
+# =>
+  Car.turn("右")
+
+#クラス
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(10)
